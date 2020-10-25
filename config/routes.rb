39a846df1a652552b4f2bb5ccab0c2fe00008b2c
eric <eric.razorjr@gmail.com>
2020-10-25 14:17:22 -0400

    Add plant route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :plants, only: [:index, :show]
+  get "api/v1/plants", to: "api/v1/plants#index"
+  # resources :plants, only: [:index, :show]
   resources :plant_cards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
