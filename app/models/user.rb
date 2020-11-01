e3d398a7e6782be91607a0ad9ee08676efb7ce48
eric <eric.razorjr@gmail.com>
2020-10-31 20:47:31 -0400

    Include password parameter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :plant_cards
     has_many :plants, through: :plant_cards
 
-    validates :email, :password presence: true
+    validates :username, :email, :password, presence: true
     validates :username, uniqueness: true
 end
