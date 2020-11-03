1b5244d4fe0201083b8fd82424449af0b4ee7635
eric <eric.razorjr@gmail.com>
2020-11-02 19:30:46 -0500

    Get user route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "api/v1/login", to: "api/v1/sessions#create"
   post "api/v1/signup", to: "api/v1/users#create"
   delete "api/v1/logout", to: "api/v1/sessions#destroy"
+  get "api/v1/user", to: "api/v1/sessions#get_user"
   
   get "api/v1/plants", to: "api/v1/plants#index"
   # resources :plants, only: [:index, :show]
