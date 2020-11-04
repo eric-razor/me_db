970000d1c00eda0f69ee336fcd8d8cd908cf21ca
eric <eric.razorjr@gmail.com>
2020-11-03 19:14:25 -0500

    Change login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post "api/v1/login", to: "api/v1/sessions#create"
+  post "api/v1/login", to: "api/v1/users#login"
   post "api/v1/signup", to: "api/v1/users#create"
   delete "api/v1/logout", to: "api/v1/sessions#destroy"
   get "api/v1/user", to: "api/v1/sessions#get_user"
