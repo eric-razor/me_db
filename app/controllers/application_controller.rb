b21f4cd433ff9020565dbe9c09b217a8f0805db4
eric <eric.razorjr@gmail.com>
2020-11-22 13:17:23 -0500

    Refactor token methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,45 +2,24 @@ class ApplicationController < ActionController::API
     before_action :authorized
     # returns the current user
 
-    def encode_token(payload)
+    def generate_token(payload)
         # generate token
-        JWT.encode(payload, 'shhh, token')
+        JWT.encode(payload, ENV['JWT_TOKEN_SECRET'])
     end
 
-    def auth_header
-      # { Authorization: 'Bearer <token>' }
-      # nil
-      request.headers['Authorization']
-    end
-
-    def decoded_token
-      if auth_header
-        token = auth_header.split(' ')[1]
-        begin
-          JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-        rescue JWT::DecodeError
-          nil
-        end
-      end
+    def decoded_token_and_user_id
+      JWT.decode(request.headers["Authorization"], ENV['JWT_TOKEN_SECRET'])[0]["id"]
     end
 
     def session_user
-      decoded_hash = decoded_token
-      if decoded_hash
-        puts decoded_hash.class
-        user_id = decoded_hash[0]['user_id']
-        @user = User.find_by(id: user_id)
-        byebug
-      else 
-        nil
-      end
+      begin
+        @session_user ||= User.find(decoded_token_and_user_id)
+      rescue
+        return nil
     end
 
     def logged_in?
         !!session_user
     end
 
-    def authorized
-        render json: {message: 'Please log in'}, status: :unauthorized unless logged_in?
-    end
 end
