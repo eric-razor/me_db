a8d5bfdfb19db65300d539984b60a21fc459d405
eric <eric.razorjr@gmail.com>
2020-11-29 12:11:41 -0500

    Redirect to auth page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,17 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   def spotify_authorization
-    byebug
-    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    query_params = {
+      client_id: ENV['SPOTIFY_CLIENT_ID'],
+      response_type: "code",
+      redirect_uri: ENV['REDIRECT_URI'],
+      scope: "user-read-email user-library-read user-modify-playback-state user-modify-playback-state",
+      show_dialog: true
+    }
+
+    url = "https://accounts.spotify.com/authorize/"
+
+    redirect_to "#{url}?#{query_params.to_query}"
 
   end
 
