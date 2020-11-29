53e57501d2d2ff25d5f4ff8c110b9f6640734796
eric <eric.razorjr@gmail.com>
2020-11-28 23:22:38 -0500

    Change to spotify authorization

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,62 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-    def create
-    @user = User.create!(signup_params)
-
-    if @user.valid?
-      payload = {user_id: @user.id}
-      token = encode_token(payload)
-      # render key value pairs of user and token 
-      render json: {user: user, jwt: token}
-    else
-      render json: { errors: user.errors.full_messages}
-    end
-  end
-
-  def spotify_login
+  def spotify_authorization
     byebug
-  end
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
 
-  def login 
-    @user = User.find_by(email: params[:email])
-
-    if @user && @user.authenticate(params[:password])
-      payload = {user_id: @user.id}
-      token = encode_token(payload)
-      render json: {user: @user, jwt: token, success: "Welcome back, #{@user.username}!"}
-    else 
-      render json: {error: "Invalid username or password"}
-    end
-  end
-
-  def auto_login
-    render json: @user
   end
 
   def logout
-    @user.destroy
+    @spotify_user.destroy
     render json: { notice: "successfully logged out"}, status: :ok
-  end
-
-   def get_user
-    if logged_in?
-      render json: {user: @user, token: @token}
-    else
-      render json: {
-        error: "No one logged in"
-      }
-    end
-   end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def signup_params
-    params.require(:user).permit(:username, :email, :password)
   end
 
 end
