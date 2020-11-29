480edbb623bb1e3dd0344feeab3d66c864741421
eric <eric.razorjr@gmail.com>
2020-11-29 12:17:52 -0500

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-
-  post "api/v1/login", to: "api/v1/users#login"
   
-  get "auth/spotify/callback", to: "api/v1/users#spotify_authorization"
-
-  post "api/v1/signup", to: "api/v1/users#create"
+  get "api/v1/login", to: "api/v1/users#spotify_authorization"
+  
   delete "api/v1/logout", to: "api/v1/users#logout"
-  get "api/v1/user", to: "api/v1/users#get_user"
-  get "api/v1/account", to: "api/v1/users#account"
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
