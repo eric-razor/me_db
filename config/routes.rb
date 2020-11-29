a8374067f70dc7cc0665f2c337301e511e47e6fa
eric <eric.razorjr@gmail.com>
2020-11-28 22:48:58 -0500

    Add spotify callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
   post "api/v1/login", to: "api/v1/users#login"
-  get "api/v1/spotify_login", to: "api/v1/users#spotify_login"
+  
+  get "auth/spotify/callback", to: "api/v1/users#spotify_authorization"
+
   post "api/v1/signup", to: "api/v1/users#create"
   delete "api/v1/logout", to: "api/v1/users#logout"
   get "api/v1/user", to: "api/v1/users#get_user"
   get "api/v1/account", to: "api/v1/users#account"
-  
-  get "api/v1/plants", to: "api/v1/plants#index"
-  # resources :plants, only: [:index, :show]
-  resources :plant_cards
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
