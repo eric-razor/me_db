d50a342b78c79ca4a01a0d170b76ed0cb1d10f04
eric <eric.razorjr@gmail.com>
2020-11-30 18:54:20 -0500

    Add Authorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,5 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
-    # returns the current user
-
-    def generate_token(payload)
-        # generate token
-        JWT.encode(payload, ENV['JWT_TOKEN_SECRET'])
+    def authorized
+    render json: {error: "Access denied: not authorized."}, status: 401 unless logged_in?
     end
-
-    def decoded_token_and_user_id
-      JWT.decode(request.headers["Authorization"], ENV['JWT_TOKEN_SECRET'])[0]["id"]
-    end
-
-    def session_user
-      begin
-        @session_user ||= User.find(decoded_token_and_user_id)
-      rescue
-        return nil
-    end
-
-    def logged_in?
-        !!session_user
-    end
-
 end
