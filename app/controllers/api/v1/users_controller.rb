fa12356bb4f8d5a20f51f9eed73dc4293320e3d4
eric <eric.razorjr@gmail.com>
2020-12-20 22:08:04 -0500

    Remove spotify login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,33 +1,37 @@
 class Api::V1::UsersController < ApplicationController
-
-  # skip_before_action :authorized, only: [:spotify_request]
-
-  def spotify_authorization
-    query_params = {
-      client_id: ENV['SPOTIFY_CLIENT_ID'],
-      response_type: "code",
-      redirect_uri: ENV['REDIRECT_URI'],
-      scope: "user-read-email user-library-read user-modify-playback-state user-modify-playback-state",
-      show_dialog: true
-    }
-
-    url = "https://accounts.spotify.com/authorize/"
-
-    redirect_to "#{url}?#{query_params.to_query}"
-
-  end
-
-  def create 
-
-  end
-
-  def show
+  before_action :login_required, :only => :my_account
+  def login 
+    byebug
+    @user = User.new
+    @user.username = params[:username]  
     byebug
   end
 
-  def logout
-    @spotify_user.destroy
-    render json: { notice: "successfully logged out"}, status: :ok
+  def process_login
+    if user = User.authenticate(params[:user])
+      #assign session id to user
+      session[:id] = user.id 
+      # redirect
+    else
+      flash[:error] = "Invalid login."
+      # redirec
+    end
   end
+
+  def my_account 
+    # find user by session id
+  end
+
+  def logout 
+    reset_session
+    flash[:message] = "Logged out."
+    # add redirect
+  end
+
+  def create
+
+  end
+
+
 
 end
