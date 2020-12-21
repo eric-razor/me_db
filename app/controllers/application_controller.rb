4dfbe1d6e3e227f49cd55f5e75c18bfb97d6846f
eric <eric.razorjr@gmail.com>
2020-12-21 16:55:07 -0500

    Create user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::API
-    def authorized
-    render json: {error: "Access denied: not authorized."}, status: 401 unless logged_in?
+    # before_action :set_user
+
+    protected
+  # runs on every action because of before_filter allowing user to be set on frontend
+    def set_user
+        @user = User.find(session[:id]) if @user.nil && session[:id]
     end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
 end
