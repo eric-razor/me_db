727b3fd539757a051c484e813368dc4b38d843eb
eric <eric.razorjr@gmail.com>
2020-12-22 12:21:05 -0500

    Signup route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # assign '/' route to get_current_user
       post "/login", to: "users#login"
+      post "/signup", to: "users#create"
       get "/user", to: "users#show"
       delete "/logout", to: "users#logout"
       resources :users, only: [:create]
