3c70a7a4e05fbcfe881d1bc9809dfff82441f260
eric <eric.razorjr@gmail.com>
2020-12-26 21:02:24 -0500

    Associate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: true, presence: true
     validates :password, length: {:in => 8..40,}, presence: true
+
+    has_many :posts
+    
 end
