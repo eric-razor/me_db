5e6f64645f7b58cae7959d00e0926c05b9a5d3fe
eric <eric.razorjr@gmail.com>
2020-12-28 18:13:14 -0500

    Fix route namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-
-
-  resources :categories
-  resources :comments
-  resources :posts
   namespace :api do
     namespace :v1 do
       # assign '/' route to get_current_user
       post "/login", to: "users#login"
       post "/signup", to: "users#create"
-      get "/user", to: "users#get_user"
       delete "/logout", to: "users#logout"
+      get "/user", to: "users#get_user"
+
       resources :users, only: [:create]
+      resources :categories
+      resources :comments
+      resources :posts
     end
   end
 end
