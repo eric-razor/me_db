144cf94046c1f02b5dc6bfb83891562fcbb0578a
eric <eric.razorjr@gmail.com>
2020-12-28 18:12:41 -0500

    Fix namespace

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::PostsController < ApplicationController
+
+    def index 
+
+    end
+
+    def create
+    
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+end
